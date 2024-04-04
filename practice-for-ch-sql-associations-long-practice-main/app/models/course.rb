93639ccc5703c09d9ffaadcf70a08cc159b830b5
Victor Cai <victorcai622@gmail.com>
2024-04-04 17:55:00 -0400

    finished project

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -14,4 +14,26 @@ class Course < ApplicationRecord
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Enrollment
+
+    has_many :enrolled_students,
+       through: :enrollments,
+       source: :student
+
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+
+    has_one :postrequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+        #instructor belongs to a course
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
 end
